8cdd071864da48851e74f28b3a9b66079cda0c63
Roman Makarov <rk_box@mail.ru>
2014-03-17 12:18:48 +0400

    favicon, logo changed, contacts page edited

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Cпециальное конструкторское бюро ”Тензор”',
+	'name'=>'Научно-производственное предприятие «Интеграл»',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
