642c82ba18719c77ef306edaf0fe5eda7f806b23
Roman Makarov <rk_box@mail.ru>
2014-08-06 16:14:37 +0400

    обновлена контактная информация

--- protected/views/site/pages/contacts.php
+++ protected/views/site/pages/contacts.php
@@ -10,21 +10,30 @@ $this->breadcrumbs=array(
 <b>Открытое акционерное общество «Научно-производственное предприятие «Интеграл»</b>
 </p>
 <p>
-Адрес: 119034, Россия,  г.Москва, ул.Пречистенка,  д.40/2,  стр.2
+119034, Россия,  г.Москва, ул.Пречистенка,  д.40/2,  стр.2
 </p>
 <p>
-Тел.: +7 (499) 255-87-48,  255-34-39
+тел./факс&nbsp;<a class="nolink black" href="tel:+7 (499) 255 87 48">+7 (499) 255 87 48</a>,&nbsp;<a class="nolink black" href="tel: 255 34 39">255 34 39</a>
 </p>
 <p>
-Факс:  +7 (499) 255-87-48,  255-34-39
+    e-mail&nbsp;<a class="nolink black" href="mailto:office@nppintegral.ru">office@nppintegral.ru</a>
 </p>
 <p>
-<br />
-<b>Руководство компании:</b>
-</p>
-<p>
-<b>Генеральный директор</b><br />Каплоухий Сергей Александрович<br />
+<b>Генеральный директор</b>&nbsp;&mdash;&nbsp;Каплоухий Сергей Александрович<br />
 </p>
 <p>
     <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=JbPqcteKqMMximgoE6Oj_2lAQqmKdibs&width=600&height=450"></script>
+</p>
+<br />
+<p>
+    <b>Филиал ОАО «НПП «Интеграл»</b>
+</p>
+<p>
+141980, Россия, г. Дубна Московской области, ул. Приборостроителей, д. 2
+</p>
+<p>
+тел./факс&nbsp;<a class="nolink black" href="tel:+7 (496) 217 03 83">+7 (496) 217 03 83</a>
+</p>
+<p>
+e-mail&nbsp;<a class="nolink black" href="mailto:ig@nppintegral.ru">ig@nppintegral.ru</a>
 </p>
