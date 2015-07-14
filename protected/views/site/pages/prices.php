9215ba74e4d325080a021d2541dc1ad3c6460766
Roman Makarov <rk_box@mail.ru>
2015-07-14 23:46:00 +0300

    prices page updated, new list for uto

--- protected/views/site/pages/prices.php
+++ protected/views/site/pages/prices.php
@@ -6,15 +6,12 @@ $this->breadcrumbs=array(
     'Цены',
 );
 ?>
+<p>
+    Цена изделия зависит от общего количества заказанных изделий, ниже приведены ссылки на соответстующие прайс-листы.
+</p>
 <h4 class="tcolor mtop05em bottom025em">Цена устройста водоограничения</h4>
 <p style="margin-bottom: 5px">
-    Цена изделия зависит от общего количества заказанных изделий, ниже приведены ссылки на соответстующие прайс-листы.
     <ul>
-    <!--
-        <li>
-            <a href="files/prices/uvo.pdf" target="_blank">Цена УВО (ТС)</a>
-        </li>
-    -->
         <li>
             <a href="files/prices/uvo1-299.pdf" target="_blank">Цена УВО (ХВС и ГВС) при заказе от 1 до 299 изделий</a>
         </li>
@@ -31,5 +28,18 @@ $this->breadcrumbs=array(
 </p>
 <h4 class="tcolor mtop05em bottom025em">Цена устройста токоограничения</h4>
 <p style="margin-bottom: 5px">
-    <a href="files/prices/uto.pdf" target="_blank">Прайс-лист на изделия группы УТО</a>
+    <ul>
+        <li>
+            <a href="files/prices/uto1-299.pdf" target="_blank">Цена УТО при заказе от 1 до 299 изделий</a>
+        </li>
+        <li>
+            <a href="files/prices/uto300-999.pdf" target="_blank">Цена УТО при заказе от 300 до 999 изделий</a>
+        </li>
+        <li>
+            <a href="files/prices/uto1000-4999.pdf" target="_blank">Цена УТО при заказе от 1000 до 4999 изделий</a>
+        </li>
+        <li>
+            <a href="files/prices/uto5000.pdf" target="_blank">Цена УТО при заказе от 5000 изделий</a>
+        </li>
+    </ul>
 </p>
