520dc4616e20fa167557a0432293a7cb2fc3ba46
rmakarov <rmakarov@appnow.com>
2015-09-09 19:15:34 +0300

    new prices and main page archive

--- protected/views/site/pages/prices.php
+++ protected/views/site/pages/prices.php
@@ -6,42 +6,19 @@ $this->breadcrumbs=array(
     'Цены',
 );
 ?>
-<!--
-<p>
-    Цена изделия зависит от общего количества заказанных изделий, ниже приведены ссылки на соответстующие прайс-листы.
-</p>
-<h4 class="tcolor mtop05em bottom025em">Цена устройста водоограничения</h4>
--->
 <p style="margin-bottom: 5px">
     <ul>
         <li>
-            <a href="files/prices/uvo1-299.pdf" target="_blank">ПРАЙС-ЛИСТ №1 на изделия группы УВО</a>
+            <a href="files/prices/uto_uvo1-299.pdf" target="_blank">ПРАЙС-ЛИСТ №1 на изделия группы УВО, УТО</a>
         </li>
         <li>
-            <a href="files/prices/uvo300-999.pdf" target="_blank">ПРАЙС-ЛИСТ №2 на изделия группы УВО</a>
+            <a href="files/prices/uto_uvo1000-4999.pdf" target="_blank">ПРАЙС-ЛИСТ №2 на изделия группы УВО, УТО</a>
         </li>
         <li>
-            <a href="files/prices/uvo1000-4999.pdf" target="_blank">ПРАЙС-ЛИСТ №3 на изделия группы УВО</a>
+            <a href="files/prices/uto_uvo300-999.pdf" target="_blank">ПРАЙС-ЛИСТ №3 на изделия группы УВО, УТО</a>
         </li>
         <li>
-            <a href="files/prices/uvo5000.pdf" target="_blank">ПРАЙС-ЛИСТ №4 на изделия группы УВО</a>
-        </li>
-    </ul>
-</p>
-<!-- <h4 class="tcolor mtop05em bottom025em">Цена устройста токоограничения</h4> -->
-<p style="margin-bottom: 5px">
-    <ul>
-        <li>
-            <a href="files/prices/uto1-299.pdf" target="_blank">ПРАЙС-ЛИСТ №1 на изделия группы УТО</a>
-        </li>
-        <li>
-            <a href="files/prices/uto300-999.pdf" target="_blank">ПРАЙС-ЛИСТ №2 на изделия группы УТО</a>
-        </li>
-        <li>
-            <a href="files/prices/uto1000-4999.pdf" target="_blank">ПРАЙС-ЛИСТ №3 на изделия группы УТО</a>
-        </li>
-        <li>
-            <a href="files/prices/uto5000.pdf" target="_blank">ПРАЙС-ЛИСТ №4 на изделия группы УТО</a>
+            <a href="files/prices/uto_uvo5000.pdf" target="_blank">ПРАЙС-ЛИСТ №4 на изделия группы УВО, УТО</a>
         </li>
     </ul>
 </p>
